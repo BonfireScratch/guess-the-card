17ae4ae3d838c2cac6280ae1ca50ac450420893e
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-03-21 18:29:46 +0200

    Update main.c

--- number_guess/main.c
+++ number_guess/main.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h> // Standard imports
 
 int main() {
-	srand(time(NULL));
+	srand(time(NULL)); // Resetting the random number generator
 	
 	int guess = 0;
 	int larger = 0;
 	int smaller = 0;
-	int num = (rand() % 201 - 100);
-	printf("%d\n", num);
-	printf("Enter an integer > ");
-	scanf("%d", &guess);
+	int num = (rand() % 201 - 100); // Sets integer num to a number between -100 to 100
+	printf("Enter an integer > "); // Asks for user input
+	scanf("%d", &guess); // Gets user input
 	
-	
-	while (guess != num) {
-		if (num < guess) {
+	while (guess != num) { // Repeats until you guess the right number
+		if (num < guess) { // If your number is too small
 			printf("Too small\n");
-			larger++;
+			larger++; // Increments integer number
 		} else {
-			printf("Too big\n");
-			smaller++;
+			printf("Too big\n"); // If number is too big
+			smaller++; // Increments integer smaller
 		}
-		printf("Enter an integer > ");
+		printf("Enter an integer > "); // Ask for input again
 		scanf("%d", &guess);
 	}
 	
-	printf("Number %d, Larger %d, Smaller %d", num, larger, smaller);
+	printf("Number %d, Larger %d, Smaller %d", num, larger, smaller); // Outputs the number you had to guess and the integers larger and smaller
 }
